1bab0af595a197e02dffba0ea53ed3705db1ef23
SoorajSNBlaze333 <sooraj@skcript.com>
2020-02-05 20:54:21 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("JS Loaded!")
+
+let matrixCells = [];
+for(var i = 0 ; i < 10 ; i++) {
+  matrixCells[i] = [0, 0, 0, 0, 0, 0, 0, 0,0 ,0];
+}
+
+let matrix = document.getElementById('matrix-wrapper');
+matrixCells.map(function(index, matrixCellCol) {
+  console.log(matrixCellCol)
+})
